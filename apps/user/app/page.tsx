6abc8528c53000d978b4334e41994686579cb8bd
Lakshay Batra <lakshaynandu.batraben10@gmail.com>
2025-12-12 15:48:41 +0530

    Recoil added completely

--- apps/user/app/page.tsx
+++ apps/user/app/page.tsx
@@ -1,10 +1,15 @@
+"use client"
+
 import { Button } from "@repo/ui/button";
+import {useBalance} from "@repo/store/hooks"
 
 export default function Home(){
+  const balance = useBalance();
   return (
     <div className="m-10">
       User landing page
       <Button />
+      {balance}
     </div>
   )
 }
